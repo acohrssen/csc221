6d0aea208e68911b4029df97ce96e6cd7dd95a42
Amelie Cohrssen <arcohrssen@gmail.com>
2023-09-30 18:27:03 -0400

    up

--- robot.py
+++ robot.py
@@ -3,3 +3,16 @@ from gasp import *
 begin_graphics()
 finished = False
 
+def place_player():
+    print("Here I am!")
+
+def move_player():
+    print("I'm moving...")
+    update_when('key_pressed')
+
+place_player()
+
+while not finished:
+    move_player()
+
+end_graphics()
